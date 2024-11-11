d67be7084a6c724b82746d5a42809d4c9757bd62
phat ho <phatho1508@gmail.com>
2024-11-11 15:43:30 +0700

    firts commit

--- futureValue/index.php
+++ futureValue/index.php
@@ -21,12 +21,38 @@
 </style>
 
 <body>
-    <form action="display_discount.php" method="POST">
+    <form action="" method="POST">
         <h1>Future Value Calculator</h1>
         Inventment Amount: <input type="text" name="inventmentAmount">
         Yearly Interest Rate: <input type="text" name="yearlyInterestRate">
         Number of Years: <input type="number" name="numberOfYears">
         <input type="submit" value="calculate">
+        <p>
+            <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (isset($_POST["inventmentAmount"]) && isset($_POST["yearlyInterestRate"]) && isset($_POST["numberOfYears"])) {
+
+                    $inventmentAmount = floatval($_POST["inventmentAmount"]);
+                    $yearlyInterestRate = floatval($_POST["yearlyInterestRate"]);
+                    $numberOfYears = floatval($_POST["numberOfYears"]);
+
+
+                    if (!($inventmentAmount >= 0 && $yearlyInterestRate >= 0 &&  $numberOfYears >= 0)) {
+                        echo "tất cả input phải >=0";
+                        exit();
+                    }
+                    $futureValue = 0;
+                    for ($i = 0; $i < $numberOfYears; $i++) {
+                        $futureValue += ($inventmentAmount + ($inventmentAmount * $yearlyInterestRate));
+                    }
+
+                    echo "Future Value :" .   $futureValue;
+                } else {
+                    echo "Thiếu dữ liệu cần thiết.";
+                }
+            }
+            ?>
+        </p>
 
     </form>
 </body>
